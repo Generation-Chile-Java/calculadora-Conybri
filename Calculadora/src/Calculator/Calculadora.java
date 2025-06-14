59aedc4dcfc19c818762bbd1579bae79a17a35bc
Constanza Valenzuela <cony@iMac-de-Constanza.local>
2025-06-13 20:17:17 -0400

    Calculadora

---
+++ Calculadora/src/Calculator/Calculadora.java
@@ -0,0 +1,35 @@
+package Calculator;
+
+import java.util.Scanner;
+
+
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Hola, esta calculadora te ayudará a resolver ejercicios básicos");
+        System.out.println("Indica el primer numero");
+        double numeroUno = teclado.nextInt();
+        System.out.println("Indica el segundo numero");
+        double numeroDos = teclado.nextInt();
+
+        System.out.println("El primer numero es: " + numeroUno +" y " + "El segundo numero es: + numeroDos");
+
+
+        System.out.println("si ambos numeros se suman el resultado es " + (numeroUno + numeroDos));
+        System.out.println("si ambos numeros se resta el resultado es " + (numeroUno - numeroDos));
+        System.out.println("si ambos se multiplican el resultado es " + (numeroUno * numeroDos));
+        System.out.println("si ambos numeros se dividen el resultado es " + (numeroUno / numeroDos));
+        System.out.println("La resta de la division del primer numero y segundo numero es " + (numeroUno % numeroDos));
+
+
+
+
+
+
+
+
+
+    }
+}
